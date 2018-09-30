b8cfcd227aed13d0ccd0b4177aca3c2770619f01
Wagner Paulo <wagstalos@hotmail.com>
2018-09-30 15:41:30 -0300

    versão 0.6 statusBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,12 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.styleLightContent();
+      
       splashScreen.hide();
       this.pushSetup();
     });
+    
   }
 
   pushSetup(){
